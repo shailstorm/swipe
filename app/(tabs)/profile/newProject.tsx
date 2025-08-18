34f1195819f30a1df16e34b1a8829e31eebdde1a
aliceyli <aliceli@nyu.edu>
2025-08-18 15:50:38 -0400

    use session to create new project

--- app/(tabs)/profile/newProject.tsx
+++ app/(tabs)/profile/newProject.tsx
@@ -1,17 +1,23 @@
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { supabase } from "@/lib/supabase";
+import { useAuthSession } from "@/providers/AuthProvider";
 import React, { useState } from "react";
 import { Alert, Button, TextInput } from "react-native";
 
 export default function NewProject() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const { session } = useAuthSession();
 
   const handleSubmit = async () => {
-    const { error } = await supabase
-      .from("projects")
-      .insert({ title: title, description: description });
+    if (!session?.user) throw new Error("No user on the session!");
+
+    const { error } = await supabase.from("projects").insert({
+      title: title,
+      description: description,
+      user_id: session?.user.id,
+    });
     if (error) {
       Alert.alert(`error: ${error.message}`);
     } else {
