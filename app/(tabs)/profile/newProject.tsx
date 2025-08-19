534645149f87ff3398146d53343f0f293f870595
aliceyli <aliceli@nyu.edu>
2025-08-19 17:16:59 -0400

    refresh query results

--- app/(tabs)/profile/newProject.tsx
+++ app/(tabs)/profile/newProject.tsx
@@ -2,18 +2,29 @@ import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { createProject } from "@/data/mutations";
 import { useAuthSession } from "@/providers/AuthProvider";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { router } from "expo-router";
 import React, { useState } from "react";
 import { Button, TextInput } from "react-native";
 
 export default function NewProject() {
+  const queryClient = useQueryClient();
+
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+
   const { session } = useAuthSession();
-  const user_id = session?.user.id;
+  const user_id = session?.user?.id;
 
   const mutation = useMutation({
     mutationFn: () => createProject({ title, description, user_id }),
+    onSuccess: () => {
+      // refresh query results to include this newly created project when we reroute back to profile tab
+      queryClient.invalidateQueries({
+        queryKey: ["myProjects", user_id],
+      });
+      router.replace("/(tabs)/profile");
+    },
   });
 
   const handleSubmit = async () => {
@@ -30,7 +41,6 @@ export default function NewProject() {
       <TextInput onChangeText={setDescription} value={description} />
       <Button title="Submit" onPress={handleSubmit} />
       {mutation.isPending ? <ThemedText>Adding...</ThemedText> : null}
-      {mutation.isSuccess ? <ThemedText>Project Created!</ThemedText> : null}
       {mutation.isError ? (
         <ThemedText>`error: ${mutation.error.message}`</ThemedText>
       ) : null}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,20 +4,54 @@ import {
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  focusManager,
+  onlineManager,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 import { useFonts } from "expo-font";
+import * as Network from "expo-network";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { useColorScheme } from "react-native";
+import { useEffect, useState } from "react";
+import type { AppStateStatus } from "react-native";
+import { AppState, Platform, useColorScheme } from "react-native";
 import "react-native-reanimated";
 
+// react-query auto refetch on reconnect for react native
+// idk if this works properly, but i would expect results to refresh if there are new results and you've come back from lost connectivity
+// https://tanstack.com/query/v5/docs/framework/react/react-native#online-status-management
+onlineManager.setEventListener((setOnline) => {
+  const eventSubscription = Network.addNetworkStateListener((state) => {
+    setOnline(!!state.isConnected);
+  });
+  return eventSubscription.remove;
+});
+
 export default function RootLayout() {
-  const queryClient = new QueryClient();
+  // create a single QueryClient instance
+  const [queryClient] = useState(() => new QueryClient());
 
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
+
+  // react-query refetch on app focus
+  // i would expect results to auto refresh if there are new results when you go back to the app
+  // https://tanstack.com/query/v5/docs/framework/react/react-native#refetch-on-app-focus
+  function onAppStateChange(status: AppStateStatus) {
+    if (Platform.OS !== "web") {
+      focusManager.setFocused(status === "active");
+    }
+  }
+
+  useEffect(() => {
+    const subscription = AppState.addEventListener("change", onAppStateChange);
+
+    return () => subscription.remove();
+  }, []);
 
   if (!loaded) {
     // Async font loading only occurs in development.
